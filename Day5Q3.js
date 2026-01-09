99103e67efae3cb2dd7b399e5bd1a10be7f3e221
VivekVerma12 <vermavivek8084@gmail.com>
2026-01-10 00:44:53 +0530

    Day5

--- Day5Q3.js
+++ Day5Q3.js
@@ -1,9 +1,18 @@
 
-function maxOccurance(arr)
-{
-   
-    
+function maxOccurance(arr) {
+
+    const freq = {};
+    let maxCount = 0;
+    let result = null;
+
+    for (const val of arr) {
+        freq[val] = (freq[val] || 0) + 1;
+        if (freq[val] > maxCount) {
+            maxCount = freq[val];
+            result = val;
+
+        }
+    }
+    return result;
 }
-console.log(maxOccurance(["login", "view", "login", "logout", "view", "login"])); // x`Output: 2
-
-
+console.log(maxOccurance([['a', 'b', 'c', 'a', 'b', 'a']])); 
